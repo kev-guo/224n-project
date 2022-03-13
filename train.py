d53131ea380632b616f75f74f80dedf1f45ed1a9
kev-guo <40834919+kev-guo@users.noreply.github.com>
2022-03-12 21:31:19 -0800

    use adam

--- train.py
+++ train.py
@@ -69,7 +69,7 @@ def main(args):
                                  log=log)
 
     # Get optimizer and scheduler
-    optimizer = optim.Adadelta(model.parameters(), args.lr,
+    optimizer = optim.Adam(model.parameters(), args.lr,
                                weight_decay=args.l2_wd)
     scheduler = sched.LambdaLR(optimizer, lambda s: 1.)  # Constant LR
 
